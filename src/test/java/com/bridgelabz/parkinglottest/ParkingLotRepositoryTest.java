3cd3ddc6a2e6ceb06e404f543dd61ae741b56f27
Raj <rajkamal11126@gmail.com>
2021-01-05 12:23:49 +0530

    Added: Functionality to test and code to take in full sign if vehicle unparked

--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotRepositoryTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotRepositoryTest.java
@@ -9,65 +9,78 @@ import com.bridgelabz.parkinglot.ParkingLot;
 import com.bridgelabz.parkinglot.ParkingLot.ParkingLotStatus;
 import com.bridgelabz.parkinglot.ParkingLotException;
 
-
 public class ParkingLotRepositoryTest {
 	Car[] carDetails = new Car[101];
-    @Before
-    public void setup() {
-        for (int i = 0; i < 100; i++) {
-            carDetails[i] = new Car()
-                    .setCarNumber("ABC " + (i+1))
-                    .setColor("Black")
-                    .setModelName("CarModel " + i)
-                    .setOwnerName("ABC's "+ i)
-                    .getCarDetails();
-        }
-    }
-    @Test
-    public void whenVehicleArrives_shouldParkInLot() {
-        ParkingLot parkingLot = new ParkingLot(carDetails);
-        boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
-        Assert.assertTrue(carParkStatus);
-    }
-    @Test
-    public void whenDriverArrives_shouldUnparkVehicle() {
-        ParkingLot parkingLot = new ParkingLot(carDetails);
-        boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
-        boolean carUnParkStatus = parkingLot.getVehicleParkedUnparked("ABC 1");
-        Assert.assertTrue(carUnParkStatus);
-    }
-    @Test
-    public void whenGivenInvalidCarNumber_shouldThrowException() {
-        try {
-            ParkingLot parkingLot = new ParkingLot(carDetails);
-            boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
-            boolean carUnParkStatus = parkingLot.getVehicleParkedUnparked("ABC 12");
-        } catch (ParkingLotException e) {
-            Assert.assertEquals(ParkingLotException.ExceptionType.NO_SUCH_CAR_NUMBER, e.type);
-        }
-    }
-    @Test
-    public void whenMoreThen100Vehicle_shouldSetParkingLotStatusFull() {
-        carDetails[carDetails.length-1] = new Car()
-                .setCarNumber("ABC 101")
-                .setColor("Black")
-                .setModelName("CarModel 101")
-                .setOwnerName("ABC's 101")
-                .getCarDetails();
-        ParkingLot parkingLot = new ParkingLot(carDetails);
-        boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
-        Assert.assertEquals(ParkingLotStatus.FULL, ParkingLot.ParkingLotStatus.FULL);
-    }
-    @Test
-    public void whenParkingLotStatusFull_shouldSetSecurityStaffStatusFull() {
-        carDetails[carDetails.length-1] = new Car()
-                .setCarNumber("ABC 101")
-                .setColor("Black")
-                .setModelName("CarModel 101")
-                .setOwnerName("ABC's 101")
-                .getCarDetails();
-        ParkingLot parkingLot = new ParkingLot(carDetails);
-        boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
-        Assert.assertEquals(ParkingLotStatus.FULL, ParkingLot.securityStatus);
-    }
+
+	@Before
+	public void setup() {
+		for (int i = 0; i < 100; i++) {
+			carDetails[i] = new Car().setCarNumber("ABC " + (i + 1)).setColor("Black").setModelName("CarModel " + i)
+					.setOwnerName("ABC's " + i).getCarDetails();
+		}
+	}
+
+	@Test
+	public void whenVehicleArrives_shouldParkInLot() {
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		Assert.assertTrue(carParkStatus);
+	}
+
+	@Test
+	public void whenDriverArrives_shouldUnparkVehicle() {
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		boolean carUnParkStatus = parkingLot.getVehicleParkedUnparked("ABC 1");
+		Assert.assertTrue(carUnParkStatus);
+	}
+
+	@Test
+	public void whenGivenInvalidCarNumber_shouldThrowException() {
+		try {
+			ParkingLot parkingLot = new ParkingLot(carDetails);
+			boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+			boolean carUnParkStatus = parkingLot.getVehicleParkedUnparked("ABC 12");
+		} catch (ParkingLotException e) {
+			Assert.assertEquals(ParkingLotException.ExceptionType.NO_SUCH_CAR_NUMBER, e.type);
+		}
+	}
+
+	@Test
+	public void whenMoreThen100Vehicle_shouldSetParkingLotStatusFull() {
+		carDetails[carDetails.length - 1] = new Car().setCarNumber("ABC 101").setColor("Black")
+				.setModelName("CarModel 101").setOwnerName("ABC's 101").getCarDetails();
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		Assert.assertEquals(ParkingLotStatus.FULL, ParkingLot.ParkingLotStatus.FULL);
+	}
+
+	@Test
+	public void whenParkingLotStatusFull_shouldSetSecurityStaffStatusFull() {
+		carDetails[carDetails.length - 1] = new Car().setCarNumber("ABC 101").setColor("Black")
+				.setModelName("CarModel 101").setOwnerName("ABC's 101").getCarDetails();
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		Assert.assertEquals(ParkingLotStatus.FULL, ParkingLot.securityStatus);
+	}
+
+	@Test
+	public void whenMoreThen100Vehicle_ifAnyVehicleUnparked_shouldSetParkingLotStatusNotFull() {
+		carDetails[carDetails.length - 1] = new Car().setCarNumber("ABC 101").setColor("Black")
+				.setModelName("CarModel 101").setOwnerName("ABC's 101").getCarDetails();
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		carParkStatus = parkingLot.getVehicleParkedUnparked("ABC 10");
+		Assert.assertEquals(ParkingLotStatus.NOT_FULL, ParkingLot.ownerParkingLotStatus);
+	}
+
+	@Test
+	public void whenParkingLotStatusFull_shouldSetSecurityStaffStatusNotFull() {
+		carDetails[carDetails.length - 1] = new Car().setCarNumber("ABC 101").setColor("Black")
+				.setModelName("CarModel 101").setOwnerName("ABC's 101").getCarDetails();
+		ParkingLot parkingLot = new ParkingLot(carDetails);
+		boolean carParkStatus = parkingLot.getVehicleParkedUnparked();
+		carParkStatus = parkingLot.getVehicleParkedUnparked("ABC 10");
+		Assert.assertEquals(ParkingLotStatus.NOT_FULL, ParkingLot.securityStatus);
+	}
 }
